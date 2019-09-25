13a0d5320e344a7708c9918cc9a23ce72faccc01
EC2 Default User <ec2-user@ip-172-31-81-22.ec2.internal>
2019-09-25 11:26:22 +0000

    added basic html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def podcaster
+  end
+  
+  def advertiser
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get '/podcasters' => 'home#podcaster'
+  get '/advertisers' => 'home#advertiser'
 end
