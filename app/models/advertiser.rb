e339543687c14253d4f958047e4f246e684a60f1
EC2 Default User <ec2-user@ip-172-31-81-22.ec2.internal>
2019-10-02 04:18:06 +0000

    update

--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -6,7 +6,7 @@ class Advertiser < ApplicationRecord
 
     has_secure_password
     
-    validate :uniqueness_of_a_property_across_models #, on: :create
+    # validate :uniqueness_of_a_property_across_models #, on: :create
     
     # def self.uniqueness_of_a_property_across_models(advertiser,email)
     #     if Advertiser.where(email: email)
@@ -17,17 +17,16 @@ class Advertiser < ApplicationRecord
     # end
     
     
-    def uniqueness_of_a_property_across_models
+    # def uniqueness_of_a_property_across_models
         
-        # binding.pry
+    #     # binding.pry
         
-        if Advertiser.where(email: :email)
-            advertiser.errors.add(:email, "このemailは使用されています")
-        elsif Podcaster.where(email: :email)
-            self.errors.add(:email, "このemailは使用されています")
-        end
-    end
-
+    #     if Advertiser.where(email: :email)
+    #         errors.add(:email, "このemailは使用されています")
+    #     elsif Podcaster.where(email: :email)
+    #         errors.add(:email, "このemailは使用されています")
+    #     end
+    # end
 
     
     # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
