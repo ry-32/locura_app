0ccf9e33d5da77a599990b6c6514fb5af0b2a6f8
EC2 Default User <ec2-user@ip-172-31-81-22.ec2.internal>
2019-10-02 10:24:17 +0000

    updates

--- app/assets/javascripts/myaccount.js
+++ app/assets/javascripts/myaccount.js
@@ -4,20 +4,30 @@ document.addEventListener("turbolinks:load", function() {
     
     $(function(){
         $('.co1').addClass('active');
+        $('#prof').addClass('active-btn');
     });
     
     
     $('#prof').on('click',function(){
+        $(this).addClass('active-btn')
+        $('#deals').removeClass('active-btn')
+        $('#payment').removeClass('active-btn')
         $('.co1').addClass('active');
         $('.co2').removeClass('active');
         $('.co3').removeClass('active');
     });
     $('#deals').on('click',function(){
+        $(this).addClass('active-btn')
+        $('#prof').removeClass('active-btn')
+        $('#payment').removeClass('active-btn')
         $('.co2').addClass('active');
         $('.co1').removeClass('active');
         $('.co3').removeClass('active');
     });
     $('#payment').on('click',function(){
+        $(this).addClass('active-btn')
+        $('#prof').removeClass('active-btn')
+        $('#deals').removeClass('active-btn')
         $('.co3').addClass('active');
         $('.co1').removeClass('active');
         $('.co2').removeClass('active');
