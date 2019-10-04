829128b36720eb64143b095bbfbe66290198d358
EC2 Default User <ec2-user@ip-172-31-81-22.ec2.internal>
2019-10-04 03:56:28 +0000

    updates

--- app/models/podcaster.rb
+++ app/models/podcaster.rb
@@ -20,6 +20,7 @@ class Podcaster < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
     validates :podcast_name, presence: true
+    validates :password, presence: true
     
     
 end
